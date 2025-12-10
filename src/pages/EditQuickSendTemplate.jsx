ffbe129d6e289914d57b25f6242f2d81869203fc
mitch fields <fields.mitch@gmail.com>
2025-12-10 00:35:03 +0000

    File changes

--- src/pages/EditQuickSendTemplate.jsx
+++ src/pages/EditQuickSendTemplate.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { base44 } from '@/api/base44Client';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { createPageUrl } from './utils';
+import { createPageUrl } from '../utils';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
--- src/pages/QuickSendTemplates.jsx
+++ src/pages/QuickSendTemplates.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { base44 } from '@/api/base44Client';
 import { useNavigate } from 'react-router-dom';
-import { createPageUrl } from './utils';
+import { createPageUrl } from '../utils';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
